325e86074a980e91af1262e50ab0b688dae123d2
jagathkrishna123 <jagathkrishna900@gmail.com>
2025-12-18 10:56:39 +0530

    last commit

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -8,7 +8,7 @@ const Brands = () => {
       <Title title="Shop By Brands" subTitle="234 New item added" align="left" />
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5 bg-slate-100/80 p-6 md:p-10 rounded-xl mt-6">
         {BRANDDATA.map((item, index) => (
-          <img key={index} src={item.image} alt="" className="w-[70px] sm:w-[80px] md:w-[90px] xl:w-58 object-contain mx-auto" />
+          <img key={index} src={item.image} alt="" className="w-[77px] sm:w-[82px] md:w-[90px] xl:w-58 object-contain mx-auto" />
         ))}
       </div>
     </div>
